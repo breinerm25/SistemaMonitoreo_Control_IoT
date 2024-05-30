587cbd981b5c7b7161426206b5fe3ef087516e45
breinerm25 <65035284+breinerm25@users.noreply.github.com>
2024-05-30 14:43:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import pandas as pd
 import streamlit as st
 from PIL import Image
 
-
 st.title('Análisis de datos de Sensores en Mi Ciudad')
 image = Image.open('Graph.png')
 st.image(image)
@@ -10,32 +9,37 @@ st.image(image)
 uploaded_file = st.file_uploader('Choose a file')
 
 if uploaded_file is not None:
-   df1=pd.read_csv(uploaded_file)
-
-   st.subheader('Perfil gráfico de la variable medida.')
-   df1 = df1.set_index('time')
-   st.line_chart(df1)
-   
-   st.write(df1)
-   st.subheader('Estadísticos básicos de los sensores.')
-   st.dataframe(df1["temperatura ESP32"].describe())
-   
-   min_temp = st.slider('Selecciona valor mínimo del filtro ', min_value=-10, max_value=45, value=23, key=1)
-   # Filtrar el DataFrame utilizando query
-   filtrado_df_min = df1.query(f"`temperatura ESP32` > {min_temp}")
-   # Mostrar el DataFrame filtrado
-   st.subheader("Temperaturas superiores al valor configurado.")
-   st.write('Dataframe Filtrado')
-   st.write(filtrado_df_min)
-   
-   max_temp = st.slider('Selecciona valor máximo del filtro ', min_value=-10, max_value=45, value=23, key=2)
-   # Filtrar el DataFrame utilizando query
-   filtrado_df_max = df1.query(f"`temperatura ESP32` < {max_temp}")
-   # Mostrar el DataFrame filtrado
-   st.subheader("Temperaturas Inferiores al valor configurado.")
-   st.write('Dataframe Filtrado')
-   st.write(filtrado_df_max)
-   
-
+    df1 = pd.read_csv(uploaded_file)
+    
+    st.subheader('Contenido del archivo cargado')
+    st.write(df1.head())  # Muestra las primeras filas del DataFrame
+    st.write('Columnas del DataFrame:', df1.columns.tolist())  # Muestra las columnas del DataFrame
+    
+    if 'time' in df1.columns:
+        st.subheader('Perfil gráfico de la variable medida.')
+        df1 = df1.set_index('time')
+        st.line_chart(df1)
+        
+        st.write(df1)
+        st.subheader('Estadísticos básicos de los sensores.')
+        st.dataframe(df1["temperatura ESP32"].describe())
+        
+        min_temp = st.slider('Selecciona valor mínimo del filtro ', min_value=-10, max_value=45, value=23, key=1)
+        # Filtrar el DataFrame utilizando query
+        filtrado_df_min = df1.query(f"`temperatura ESP32` > {min_temp}")
+        # Mostrar el DataFrame filtrado
+        st.subheader("Temperaturas superiores al valor configurado.")
+        st.write('Dataframe Filtrado')
+        st.write(filtrado_df_min)
+        
+        max_temp = st.slider('Selecciona valor máximo del filtro ', min_value=-10, max_value=45, value=23, key=2)
+        # Filtrar el DataFrame utilizando query
+        filtrado_df_max = df1.query(f"`temperatura ESP32` < {max_temp}")
+        # Mostrar el DataFrame filtrado
+        st.subheader("Temperaturas Inferiores al valor configurado.")
+        st.write('Dataframe Filtrado')
+        st.write(filtrado_df_max)
+    else:
+        st.error("La columna 'time' no existe en el archivo cargado. Por favor, verifica el archivo.")
 else:
- st.warning('Necesitas cargar un archivo csv excel.')
+    st.warning('Necesitas cargar un archivo csv excel.')
